8a5197ad3d55402f6f2d9facd6d6981e9fbf0577
Guychu26 <goldenbergguy@gmail.com>
2023-02-26 19:15:32 +0200

    2023isde1 Comp V2

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
   { "name": "Event",
     "code": "e",
     "type": "event",
-    "defaultValue": "2022tur",
+    "defaultValue": "2023isde1",
     "required": "true",
     "disabled": "true"
   },
