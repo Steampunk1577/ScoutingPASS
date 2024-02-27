4bf02dd4212a5fc8defeb0239a6d3257056c6ae7
Guychu26 <goldenbergguy@gmail.com>
2024-02-27 21:35:26 +0200

    V26 2024 | Didnt Work May Be Now?

--- resources/js/scoutingPASS.js
+++ resources/js/scoutingPASS.js
@@ -5,7 +5,7 @@ document.addEventListener("touchstart", startTouch, false);
 document.addEventListener("touchend", moveTouch, false);
 // Swipe Up / Down / Left / Right
 var initialX = null;
-var xThreshold = 0.3;
+var xThreshold = 0;
 var slide = 0;
 var enableGoogleSheets = false;
 var checkboxAs = 'YN';
@@ -1201,6 +1201,15 @@ function moveTouch(e) {
   var currentX = e.changedTouches[0].screenX;
   var diffX = initialX - currentX;
 
+  // sliding horizontally
+  if (diffX / screen.width > xThreshold) {
+    // swiped left
+    swipePage(1);
+  } else if (diffX / screen.width < -xThreshold) {
+    // swiped right
+    swipePage(-1);
+  }
+  initialX = null;
 };
 
 function swipePage(increment){
